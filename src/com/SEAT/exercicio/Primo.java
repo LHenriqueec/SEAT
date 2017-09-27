726497c7ec6d50296d6d09d5c7a324b0c8b315cd
Luiz Henrique <luizhenrique.ecl@gmail.com>
2017-09-27 12:21:37 -0300

    Exercício 7: Números Primos

--- src/com/SEAT/exercicio/Primo.java
+++ src/com/SEAT/exercicio/Primo.java
@@ -1,10 +1,22 @@
 package com.SEAT.exercicio;
 
+/**
+ * Classe que trabalha com números primos
+ * @author Luiz Henrique
+ **/
 public class Primo {
 	
+	/**
+	 * 6 primeiros números primos
+	 */
 	private final static int[] primos = {2, 3, 5, 7, 11, 13};
 
 	
+	/**
+	 * 
+	 * @param value N-ésimo valor que se deseja encontrar
+	 * @return N-ésimo Nº primo encontrado
+	 */
 	public long foundNPrimo(int value) {
 		if(value <= primos.length && value > 0) {
 			return primos[value - 1];
@@ -20,7 +32,12 @@ public class Primo {
 		return count;
 	}
 	
-	public boolean isPrimo(long numero) {
+	/**
+	 * 
+	 * @param numero Valor que se deseja verificar se é primo
+	 * @return <code>true</code> caso seja um número primo
+	 */
+	private boolean isPrimo(long numero) {
 		for (int primo : primos) {
 			if(primo == numero) return true;
 			
